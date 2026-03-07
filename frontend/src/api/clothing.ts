b49c2b7cd756ef9a92aa9cdcf4bfc61204a1e556
RjGutierrezz <rbg002@uark.edu>
2026-03-06 16:47:51 -0800

    connected frontend and backend

---
+++ frontend/src/api/clothing.ts
@@ -0,0 +1,22 @@
+// API helper: to centralize backend calls
+
+
+export type ClothingItems = {
+  id: string
+  name: string
+  category: string
+  color: string
+  createdAt: string
+}
+
+
+const BASE_URL = "http:localhost:4000"
+
+export async function getClothingItems(): Promise<ClothingItems[]> {
+  const res = await fetch(`${BASE_URL}/api/clothing`)
+  if (!res.ok) {
+    throw new Error("Failed to fetch clothing items")
+  }
+  const json = await res.json()
+  return json.data
+}
--- frontend/src/components/WardrobeClothes.tsx
+++ frontend/src/components/WardrobeClothes.tsx
@@ -1,10 +1,53 @@
 import ClothingCard from "./ClothingCard"
-import { wardrobeClothesPlaceholder } from "../constants"
+
+// dont need this anymore
+// import { wardrobeClothesPlaceholder } from "../constants"
+
+import { getClothingItems, type ClothingItems } from "../api/clothing"
+import { useEffect, useState } from "react"
 
 const WardrobeClothes = () => {
+
+  // starts an empty array
+  const [ items, setItems ] = useState<ClothingItems[]>([])
+
+  const [ loading, setLoading ] = useState(true)
+  const [ error, setError ] = useState<string | null>(null)
+
+  useEffect(() => {
+
+    // To use async inside useEffect you have to do the ff
+    const run = async () => {
+      try {
+        // check to see if the data is there
+        const data = await getClothingItems()
+
+        // re-render the array to add the data to the array
+        setItems(data)
+      } catch (err){
+        setError("Could not load the wardrobe items.")
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    run()
+  }, [])
+
+
+  // checking error cases
+  if (loading) {
+    return <p>Loading wardobe...</p>
+  } else if (error) {
+    return <p>{error}</p>
+  } else if (items.length === 0) {
+    return <p>There are no clothes yet 😓. Start uploading!</p>
+  }
+
   return (
     <section className="clothing-grid">
-      {wardrobeClothesPlaceholder.map((clothes) => (
+      {/* to loop through each items and re-render each card */}
+      {items.map((clothes) => (
         <ClothingCard 
           key={clothes.id}
           name={clothes.name}
